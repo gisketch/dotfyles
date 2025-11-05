faf333794f905e683a559cd318d8ce675dc43c7c
gisketch <dev@gisketch.com>
2025-11-05 13:55:42 +0800

    update oil conflict

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -45,7 +45,7 @@ return {
                 ["<CR>"] = "actions.select",
                 ["<C-s>"] = { "actions.select", opts = { vertical = true } },
                 ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
-                ["<C-t>"] = { "actions.select", opts = { tab = true } },
+                -- ["<C-t>"] = { "actions.select", opts = { tab = true } },
                 ["<C-p>"] = "actions.preview",
                 ["<C-c>"] = "actns.close",
                 ["<C-l>"] = "actions.refresh",
